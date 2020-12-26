5ef9a2a93cb15b8fb26ac861e889d495b7bc7607
Thomas B <tbouyer2000@gmail.com>
2020-12-26 11:36:30 +0100

    packaging setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r", encoding="UTF-8") as f:
+    requires = f.read().split("\n")
+
+setuptools.setup(
+    name="phenyx",
+    version="0.1.0",
+    author="Thomas B",
+    author_email="tbouyer2000@gmail.com",
+    description="An engine for pygame",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Thomas2-bot/pygame_engine.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=requires,
+)
