a11d8cbaaa24b596d92d91413a92303a2ea6dfbf
Thomas B <tbouyer2000@gmail.com>
2021-05-27 16:15:22 +0200

    some test files

--- examples/simple_collision.py
+++ examples/simple_collision.py
@@ -1,31 +1,53 @@
 from phoenyx import *
+import random as rd
 
 renderer: Renderer = Renderer(600, 600, "collision")
-sandbox: SandBox = SandBox(renderer, 300, 300, bounce=True)
+sandbox: SandBox = SandBox(renderer, 300, 300)
+
+count = 0
+fall = True
+
+
+def revert() -> None:
+    global fall
+    fall = not fall
+
+
+def clear() -> None:
+    sandbox.clear()
+    init()
+
+
+def init() -> None:
+    for i in range(11):
+        for j in range(6):
+            sandbox.add_ball(60*i + 30 * (j%2), 100 + 60*j, 1, 15, elasticity=.99, is_static=True)
+
+    sandbox.add_segment((0, 600), (600, 600), 1, 5, is_static=True)
+    for i in range(11):
+        sandbox.add_segment((60 * i, 600), (60 * i, 500), 1, 5, is_static=True)
 
 
 def setup() -> None:
-    ball_opts = {"friction": .99, "elasticity": .99}
-    seg_opts = {"friction": .99, "elasticity": .8, "is_static": True}
-    poly_opts = {"friction": .99, "elasticity": 0}
-
-    sandbox.add_ball(300, 40, 1, 10, **ball_opts)
-    sandbox.add_ball(295, 80, 1, 10, **ball_opts)
-    sandbox.add_ball(305, 60, 1, 10, **ball_opts)
-    sandbox.add_segment((50, 500), (550, 100), 2, 5, **seg_opts)
-    sandbox.add_poly([(100, 100), (190, 100), (100, 190)], 2, **poly_opts)
-
-    sandbox.set_gravity(y=900)
+    global fall
+    init()
+    renderer.create_menu("options", toggle=revert, clear=clear)
 
     renderer.set_background(51)
     renderer.text_size = 15
-    renderer.text_color = 255
 
 
 def draw() -> None:
-    sandbox.step()
+    global count, fall
+    sandbox.step(iter=100)
     sandbox.draw()
     renderer.text(10, 10, f"fps : {round(renderer.fps)}")
+
+    count += 1
+    if count >= 30:
+        count = 0
+        if fall:
+            sandbox.add_ball(rd.randint(299, 301), 0, 1, 10, elasticity=.8)
 
 
 if __name__ == "__main__":
