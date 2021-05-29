5b0231e1fce1bee5166a1634a15fbbe934898626
Thomas B <tbouyer2000@gmail.com>
2021-05-29 21:40:48 +0200

    python packaging setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="phoenyx",
-    version="0.3.1",
+    version="0.3.2",
     author="Thomas Byr",
     author_email="thomas-c2000@outlook.fr",
     description="A drawing and physics engine for Pygame",
