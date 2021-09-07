6479654b377e3dcffbf5112afed7991a66185c75
Thomas Byr <tbouyer2000@gmail.com>
2021-09-07 19:49:48 +0200

    Phoenyx - Pygame Engine v0.3.4

--- phoenyx/utils/prime_factors.py
+++ phoenyx/utils/prime_factors.py
@@ -1,7 +1,12 @@
 __all__ = ["prime_factors"]
 
+from typing import Union
 
-def prime_factors(x: int) -> list[str]:
+
+def prime_factors(
+    x: int,
+    dtype: type[Union[list, dict]] = dict,
+) -> Union[list[int], dict[int, int]]:
     """
     a list of positive primes integers
 
@@ -9,35 +14,38 @@ def prime_factors(x: int) -> list[str]:
     ----------
         x : int
             number to check
+        dtype : type
+            type of returned data
 
     Returns
     -------
-        list[str] : primes
+        list[int] | dict[int, int] : either a list of counted primes factors or a dict of primes factors and their counts
     """
-    a, res = 0, []
+    a = 0
+    d: dict[int, int] = {}
     while x % 2 == 0:
         x //= 2
         a += 1
-    if a >= 2:
-        res.append('2^' + str(a))
-    if a == 1:
-        res.append('2')
+    if a >= 1:
+        d[2] = a
     b = 3
     while b * b <= x:
         a = 0
         while x % b == 0:
             x //= b
             a += 1
-        if a >= 2:
-            res.append(str(b) + '^' + str(a))
-        if a == 1:
-            res.append(str(b))
+        if a >= 1:
+            d[b] = a
         b += 2
     if x != 1:
-        res.append(str(x))
-    return res
+        d[x] = 1
+
+    if dtype == dict:
+        return d
+    else:
+        return sorted([k for k, v in d.items() for _ in range(v)])
 
 
 # if __name__ == '__main__':
 #     number = int(eval(input("n = ")))
-#     print(prime_factors(number))
+#     print(prime_factors(number, dtype=dict))
